d8231cd34c9da191624a703ad5304e96166c495c
mas302 <mas302@cam.ac.uk>
2024-04-06 22:21:21 +0100

    making belief propagation work

---
+++ localisation/src/discretise.py
@@ -0,0 +1,10 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+from scipy.integrate import dblquad
+
+def discretise(limits, cardinalities):
+
+    probabilities = np.zeros(limits)
+
+if __name__ == '__main__':
+    print(np.zeros([1,2,3]))
