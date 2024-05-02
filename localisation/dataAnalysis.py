d806ae889419920d9435325abfd1c8ff7ebb8e8f
mas302 <mas302@cam.ac.uk>
2024-05-02 12:40:43 +0100

    loading data + visualising trends

--- localisation/dataAnalysis.py
+++ localisation/dataAnalysis.py
@@ -1,7 +1,23 @@
 import pandas as pd
 import sys
+import seaborn as sns
+import matplotlib.pyplot as plt
+import re
 
 sys.path.insert(1, '/Users/mariam/Downloads/IIB Project readings/data_cleaned_adapted_features_full')
 
 df = pd.read_csv('/Users/mariam/Downloads/IIB Project readings/data_cleaned_adapted_features_full/sensoringData_feature_prepared_20_19.0_0.csv')
-print(df.head())
+
+columns = df.columns.values.tolist()
+columnsAsStr = ' '.join(columns)
+x = re.findall("[A-Za-z0-9]+_[A-Za-z0-9]+_mean", columnsAsStr)
+x +=['user', 'activity']
+rslt_df = df[x]
+# print(rslt_df.head)
+
+# correlation_matrix = rslt_df.iloc[:, :-1].corr(method='spearman')
+# sns.heatmap(correlation_matrix, cmap='coolwarm')
+# plt.title('Correlation Matrix')
+# plt.show()
+sns.pairplot(rslt_df)
+plt.show()
