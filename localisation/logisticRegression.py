618257c267ace352cc09b14c5f8d349c6fe18396
mas302 <mas302@cam.ac.uk>
2024-05-09 10:11:14 +0100

    trying to improve the performance of log reg

--- localisation/logisticRegression.py
+++ localisation/logisticRegression.py
@@ -11,7 +11,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.metrics import classification_report
 
-
 sys.path.insert(1, '/Users/mariam/Downloads/IIB Project readings/data_cleaned_adapted_features_full')
 sys.path.insert(1, '/Users/mariam/Downloads/IIB Project readings/IIB project code/localisation/src')
 
@@ -24,6 +23,7 @@ random.seed(0)
 columns = df.columns.values.tolist()
 columnsAsStr = ' '.join(columns)
 x = re.findall("gps+_[A-Za-z0-9]+_mean", columnsAsStr)
+# x += re.findall("acc+_[A-Za-z0-9]+_mean", columnsAsStr)
 x += ['activity']
 
 # Extract unique user IDs
@@ -31,17 +31,19 @@ userIDs = df['user'].unique().tolist()
 
 parameters = df[x]
 encodedParameters = pd.get_dummies(parameters, dtype=float)
+encodedParameters.drop(['activity_Walking', 'activity_Driving', 'gps_lat_mean', 'gps_bearing_mean'], axis=1, inplace=True)
+
 rowCount = encodedParameters.shape[0]
 midRows = math.ceil((rowCount-1)/2)
 
 # know that a user id and activity defines a session. take haversine distances of time series for consistent behaviour.
 encodedParameters['networkLat'] = np.nan
 encodedParameters['networkLat'][:midRows] = df['gps_lat_mean'][:midRows]
-encodedParameters['networkLat'][midRows:rowCount] = df['gps_lat_mean'][midRows:rowCount] + np.random.rand(rowCount- midRows)
+encodedParameters['networkLat'][midRows:rowCount] = df['gps_lat_mean'][midRows:rowCount] + 1e-3*np.random.randn(rowCount- midRows)
 
 encodedParameters['networkLon'] = np.nan
 encodedParameters['networkLon'][:midRows] = df['gps_long_mean'][:midRows]
-encodedParameters['networkLon'][midRows:rowCount] = df['gps_long_mean'][midRows:rowCount] + np.random.rand(rowCount- midRows)
+encodedParameters['networkLon'][midRows:rowCount] = df['gps_long_mean'][midRows:rowCount] + 1e-3*np.random.randn(rowCount- midRows)
 
 encodedParameters['targetVal'] = np.nan
 encodedParameters['targetVal'][:midRows] = np.ones((midRows))#df['gps_long_mean'][:500]
@@ -53,10 +55,11 @@ targetVars = encodedParameters.columns[-1]
 X_train, X_test, y_train, y_test = train_test_split(encodedParameters[dependentVars], encodedParameters[targetVars], test_size=0.2, random_state=16)
 
 # instantiate the model (using the default parameters)
-logreg = LogisticRegression(solver = 'saga', penalty= 'l2', random_state=16, verbose=1)
+logreg = LogisticRegression(solver = 'saga', max_iter=10000, penalty='l2', random_state=16, verbose=1)
 
 # fit the model with data
 logreg.fit(X_train, y_train)
+print(logreg.coef_)
 
 y_pred = logreg.predict(X_test)
 
